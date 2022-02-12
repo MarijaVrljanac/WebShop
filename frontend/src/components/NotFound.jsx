e7734804482480eda657153ee68d07470ca71117
Marija Vrljanac <marijavrljanac@gmail.com>
2022-02-12 17:24:18 +0100

    Sitne izmene

--- frontend/src/components/NotFound.jsx
+++ frontend/src/components/NotFound.jsx
@@ -7,7 +7,7 @@ function NotFound( ) {
        <div className='notfound'>
        <h1>404</h1>
        <h3>Not Found</h3>
-       <img src='https://covid19.mohp.gov.np/images//sad.png'></img>
+       <img className='smajli' src='https://covid19.mohp.gov.np/images//sad.png'></img>
        <h5>Stranica koju tražite ne postoji ili je uklonjena.</h5>
        <Link to="/"><span>Vrati se nazad na početnu stranu</span></Link>
       </div>
